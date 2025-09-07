40e4413371b2854fd8bfde7c076f4cde255f5656
Mohamed Jasim <spk@mjcomputer.local>
2025-09-07 23:39:29 +0300

    new

--- apps/api/src/health.controller.ts
+++ apps/api/src/health.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+
 @Controller('health')
 export class HealthController {
-  @Get() get() { return { ok: true, ts: new Date().toISOString() }; }
+  @Get()
+  get() {
+    return { ok: true, ts: new Date().toISOString() };
+  }
 }
